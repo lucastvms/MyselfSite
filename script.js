1f4fbc2a3a2a17e6cea324c75f3d893a127ea756
Lucas Vieira <lucas.tvms@gmail.com>
2020-07-09 23:33:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAprendizado() {
+  var div = document.getElementById("aprendizado");
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+}
